67e427a5f7088fda970ba8845e5943d719e095ae
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-05-26 11:00:47 +0300

    SUkurtas projektas. Sukurta Filmas klase, jai getteriai ir setteriai. Sukurti du bandomiejo objektai pagrindineje programoje, kurio dali informacijos isvedu i konsole

---
+++ FilmoKlasesProjektas/Filmas.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmoKlasesProjektas
+{
+    internal class Filmas
+    {
+        // Klasės kintamieji
+        private string pavadinimas;
+        private string zanras;
+        private int trukme;
+        private string rezisierius;
+        private int amziausCenzas;
+        private double reitingas;
+
+        // Konstruktoriai 
+        // PILNAS konstruktorius
+        public Filmas(string pavadinimas, string zanras, int trukme, string rezisierius, int amziausCenzas, double reitingas)
+        {
+            this.pavadinimas = pavadinimas;
+            this.zanras = zanras;
+            this.trukme = trukme;
+            this.rezisierius = rezisierius;
+            this.amziausCenzas = amziausCenzas;
+            this.reitingas = reitingas;
+        }
+
+        // TUŠČIAS konstruktrius
+        public Filmas()
+        {
+        }
+
+        // GALI BŪTI DAR KITOKIŲ KONSTRUKTORIŲ, JEIGU JŲ REIKIA.
+
+        // TOLIAU BŪTŲ GETTERIAI SETTERIAI
+        public string Pavadinimas 
+        {
+            get
+            {
+                return pavadinimas;
+            }
+            set
+            {
+                this.pavadinimas = value;
+            }
+        }
+        public string Zanras
+        {
+            get
+            {
+                return zanras;
+            }
+            set
+            {
+                zanras = value;
+            }
+        }
+        public int Trukme 
+        {
+            get { return trukme; }
+            set { trukme = value; }
+        }
+        public string Rezisierius 
+        {
+            get { return rezisierius; }
+            set { rezisierius = value; }
+        }
+        public double Reitingas 
+        {
+            get { return reitingas;}
+            set { reitingas = value; }
+        }
+        public int AmziausCenzas 
+        {
+            get { return amziausCenzas;}
+            set { amziausCenzas = value; }
+        }
+
+        // TOLIAU BŪTŲ KITI KLASĖS METODAI/FUNKCIJOS
+
+
+    }
+}
---
+++ FilmoKlasesProjektas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FilmoKlasesProjektas // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sukuriamas naujas tuščias filmas.
+            Filmas filmas = new Filmas();
+            
+            // Pabandau išvesti filmą į konsolę.
+            Console.WriteLine(filmas.ToString);
+
+            Console.WriteLine("Išveda tik klasės pavadinimą, tačiau pačio objekto duomenų - nieko neišveda. TIESA, jis yra tuščias.");
+
+
+            // SUSIKURIU PILNĄ FILMĄ
+            Filmas filmas2 = new Filmas("Baisi naktis", "Siaubo", 102, "Vilandas Znamenskas", 18, 3.6);
+
+            // Pabandau išvesti (netuščią) filmą
+            Console.WriteLine(filmas2.ToString());
+
+            Console.WriteLine("Išveda vėlgi tik klasės pavadinimą, nors ir filmas yra PILNAS (su duomenimis), bet tokie išvedimai klasių objektams netiks.... KOL NEPERSIDARYSITE ToString() funkcijos. Apie tai sekančiam projekto pavyzdyje");
+
+
+            // TOLIAU, BANDOME GAUTI PO VIENĄ FILMO DUOMENĮ IR JĮ IŠVESTI Į KONSOLĘ
+            Console.WriteLine("Filmo pavadinimas: " + filmas2.Pavadinimas);
+            Console.WriteLine("Filmo zanras: " + filmas2.Zanras);
+            Console.WriteLine("Filmo trukmė: " + filmas2.Trukme);
+        }
+    }
+}
